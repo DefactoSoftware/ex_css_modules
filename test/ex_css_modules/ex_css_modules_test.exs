f69f4ddf41f7985112c8a052034b75bc543f439b
Jesse Dijkstra <mail@jessedijkstra.nl>
2017-10-06 15:11:32 +0200

    Add code coverage

--- test/ex_css_modules/ex_css_modules_test.exs
+++ test/ex_css_modules/ex_css_modules_test.exs
@@ -80,4 +80,21 @@ defmodule ExCSSModulesTest do
       }
     end
   end
+
+  describe "class_selector/2" do
+    test "returns the selector if the key is present" do
+      assert ExCSSModules.class_selector(@example_stylesheet, "title")
+        == "._namespaced_title"
+    end
+
+    test "returns nil if the key is not present" do
+      assert ExCSSModules.class_selector(@example_stylesheet, "foo")
+        == nil
+    end
+
+    test "returns a joined string if the key is a list" do
+      assert ExCSSModules.class_selector(@example_stylesheet, ["title", "paragraph"])
+        == "._namespaced_title._namespaced_paragraph"
+    end
+  end
 end
